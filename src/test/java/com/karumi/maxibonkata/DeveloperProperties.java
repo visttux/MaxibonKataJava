9735bb26c5d0539c20bdff0eb7f0407437cbf2ab
Victor Martinez Legaz <victor.martinez@redbooth.com>
2017-03-25 12:31:56 +0100

    test KarumiHQs

--- src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
+++ src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
@@ -3,6 +3,7 @@ package com.karumi.maxibonkata;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static junit.framework.TestCase.assertTrue;
@@ -11,12 +12,27 @@ import static junit.framework.TestCase.assertTrue;
 public class DeveloperProperties {
     private static final String ANY_NAME = "Pedro";
 
-
     @Property
     public void theNumberOfMaxibonsShouldBeGreaterThanZero(int numberOfMaxibons) {
         Developer developer = new Developer(ANY_NAME, numberOfMaxibons);
 
-        System.out.println(developer);
-        assertTrue(developer.getNumberOfMaxibonsToGrab() >= 0);
+        int numberOfMaxibonsToGrab = developer.getNumberOfMaxibonsToGrab();
+
+        assertTrue(numberOfMaxibonsToGrab >= 0);
+    }
+
+    @Test
+    public void ifDeveloperIsPedroShouldGrab3Maxibons() {
+        Developer pedro = Karumies.PEDRO;
+        Developer alberto = Karumies.ALBERTO;
+        Developer davide = Karumies.DAVIDE;
+        Developer jorge = Karumies.JORGE;
+        Developer sergio = Karumies.SERGIO;
+
+        assertTrue(pedro.getNumberOfMaxibonsToGrab() == 3);
+        assertTrue(alberto.getNumberOfMaxibonsToGrab() == 1);
+        assertTrue(davide.getNumberOfMaxibonsToGrab() == 0);
+        assertTrue(jorge.getNumberOfMaxibonsToGrab() == 1);
+        assertTrue(sergio.getNumberOfMaxibonsToGrab() == 2);
     }
 }
---
+++ src/test/java/com/karumi/maxibonkata/KarumiHQsProperties.java
@@ -0,0 +1,41 @@
+package com.karumi.maxibonkata;
+
+import com.pholser.junit.quickcheck.From;
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.mock;
+
+@RunWith(JUnitQuickcheck.class)
+public class KarumiHQsProperties {
+    private KarumiHQs hq;
+
+    @Before
+    public void setup() {
+        Chat chat = mock(Chat.class);
+        hq = new KarumiHQs(chat);
+    }
+
+    @Property
+    public void numberOfMaxibonsShouldBeGreaterThanTwo(@From(DevelopersGenerator.class) Developer developer) {
+        hq.openFridge(developer);
+
+        int maxibonsLeft = hq.getMaxibonsLeft();
+
+        assertTrue(maxibonsLeft >= 2);
+    }
+
+    @Property
+    public void numberOfMaxibonsShouldBeGreaterThanTwoWhenNotSoHungry(@From(NotSoHungryDevelopersGenerator.class) Developer developer) {
+        hq.openFridge(developer);
+
+        int maxibonsLeft = hq.getMaxibonsLeft();
+
+        assertTrue(maxibonsLeft >= 2);
+    }
+
+}
