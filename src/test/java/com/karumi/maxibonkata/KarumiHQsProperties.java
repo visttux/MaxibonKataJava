88686abaeb4a59c13a7802fc1bac32bfa62354fd
Victor Martinez Legaz <victor.martinez@redbooth.com>
2017-03-25 13:17:53 +0100

    verify calls to chat

--- src/test/java/com/karumi/maxibonkata/KarumiHQsProperties.java
+++ src/test/java/com/karumi/maxibonkata/KarumiHQsProperties.java
@@ -10,15 +10,19 @@ import org.junit.runner.RunWith;
 import java.util.List;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 @RunWith(JUnitQuickcheck.class)
 public class KarumiHQsProperties {
     private KarumiHQs hq;
+    private Chat chat;
 
     @Before
     public void setup() {
-        Chat chat = mock(Chat.class);
+        chat = mock(Chat.class);
         hq = new KarumiHQs(chat);
     }
 
@@ -51,4 +55,24 @@ public class KarumiHQsProperties {
 
         assertTrue(maxibonsLeft >= 2);
     }
+
+    @Property
+    public void messageToBuyMoreIsSendWhenMaxibonsUnderTwoOnFridgeOpened(
+            @From (HungryDevelopersGenerator.class) Developer developer) {
+        hq.openFridge(developer);
+
+        String expectedResult = "Hi guys, I'm " + developer.getName() + ". We need more maxibons!";
+        verify(chat).sendMessage(expectedResult);
+    }
+
+    @Property
+    public void messageToBuyMoreIsNotSendWhenMaxibonsNotUnderTwoOnFridgeOpened(
+            @From (NotSoHungryDevelopersGenerator.class) Developer developer) {
+        hq.openFridge(developer);
+
+        String expectedResult = "Hi guys, I'm " + developer.getName() + ". We need more maxibons!";
+        verify(chat, never()).sendMessage(expectedResult);
+    }
+
+
 }
